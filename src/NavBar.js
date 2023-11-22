895bb2adda5b99515b2d0f62abe9d315bbd05e72
anto-sch <antonia.schlieder@t-online.de>
2023-11-22 16:50:53 +0100

    full screen mode

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,20 @@
 import { Link } from "react-router-dom"
-const Navbar =()=>{
+
+const toggleFullSceen = () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    }
+  };
+
+
+const Navbar = () =>{
       return (
             <div>
+                  <button onClick={() => toggleFullSceen()}>Toggle full screen mode</button>
                   <Link to="/">Home</Link>
                   <Link to="/task-1">Task 1</Link>
                   <Link to="/task-2">Task 2</Link>
