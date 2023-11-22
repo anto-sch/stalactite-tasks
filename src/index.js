8a0b42b2d3b7e6aaf792a5e7c737c199a0a9f4d3
anto-sch <antonia.schlieder@t-online.de>
2023-11-22 15:12:54 +0100

    edit routing

--- src/index.js
+++ src/index.js
@@ -11,19 +11,19 @@ import task_3 from './data/task_3.json';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/stalactite-tasks",
     element: <div>Hello world!</div>,
   },
   {
-    path: "/task-1",
+    path: "/stalactite-tasks/task-1",
     element: <StalactitePlot hierarchical_data={task_1}/>,
   },
   {
-    path: "/task-2",
+    path: "/stalactite-tasks/task-2",
     element: <StalactitePlot hierarchical_data={task_2}/>,
   },
   {
-    path: "/task-3",
+    path: "/stalactite-tasks/task-3",
     element: <StalactitePlot hierarchical_data={task_3}/>,
   }
 ]);
